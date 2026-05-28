2f4b3d1adb0846baec8c7ae92bb8ced7e0912014
MohsenBokaei <129049349+MohsenBokaei@users.noreply.github.com>
2026-05-27 23:18:12 -0500

    Refactor particle simulation and adjust parameters

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ const sketch = (p) => {
     let particles = [];
     let fluid;
     let hands;
-    let greenCenter = null; // Right
-    let blueCenter = null;  // Left
+    let greenCenter = null; 
+    let blueCenter = null;  
     let prevGreen = null;
     let prevBlue = null;
 
@@ -16,45 +16,50 @@ const sketch = (p) => {
         p.createCanvas(1600, 900);
         bgImg.resize(p.width, p.height);
         
-        // Initialize the New Motion Engine
         fluid = new FluidSimulation(p, p.width, p.height);
 
-        // Create Particles (Density 12)
         bgImg.loadPixels();
-        for (let y = 0; y < bgImg.height; y += 12) {
-            for (let x = 0; x < bgImg.width; x += 12) {
+        // Slightly higher density for Anadol look
+        for (let y = 0; y < bgImg.height; y += 10) {
+            for (let x = 0; x < bgImg.width; x += 10) {
                 let i = (x + y * bgImg.width) * 4;
-                let c = p.color(bgImg.pixels[i], bgImg.pixels[i+1], bgImg.pixels[i+2], 200);
+                let c = p.color(bgImg.pixels[i], bgImg.pixels[i+1], bgImg.pixels[i+2], 180);
                 particles.push(new ImageParticle(p, x, y, c));
             }
         }
 
-        // Camera & MediaPipe Setup (Same as your working version)
         cam = p.createCapture(p.VIDEO);
         cam.size(640, 480); cam.hide();
+        
         hands = new Hands({locateFile: (f) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${f}`});
         hands.setOptions({maxNumHands: 2, modelComplexity: 0, minDetectionConfidence: 0.5});
         hands.onResults(onResults);
-        new Camera(cam.elt, {onFrame: async () => {await hands.send({image: cam.elt})}, width: 640, height: 480}).start();
+        
+        new Camera(cam.elt, {
+            onFrame: async () => { await hands.send({image: cam.elt}) },
+            width: 640, height: 480
+        }).start();
     };
 
     function onResults(res) {
+        greenCenter = null;
+        blueCenter = null;
         if (res.multiHandLandmarks && res.multiHandedness) {
             for (let i = 0; i < res.multiHandLandmarks.length; i++) {
                 let label = res.multiHandedness[i].label;
                 let pos = p.createVector((1 - res.multiHandLandmarks[i][8].x) * p.width, res.multiHandLandmarks[i][8].y * p.height);
                 
-                if (label === "Left") { // User Right Hand
+                if (label === "Left") { // Right Hand
                     if (prevGreen) {
                         let force = p5.Vector.sub(pos, prevGreen);
-                        fluid.addForce(pos.x, pos.y, force.x, force.y, 100);
+                        fluid.addForce(pos.x, pos.y, force.x, force.y, 120);
                     }
                     greenCenter = pos;
                     prevGreen = pos.copy();
-                } else { // User Left Hand
+                } else { // Left Hand
                     if (prevBlue) {
                         let force = p5.Vector.sub(pos, prevBlue);
-                        fluid.addForce(pos.x, pos.y, force.x, force.y, 150);
+                        fluid.addForce(pos.x, pos.y, force.x, force.y, 120);
                     }
                     blueCenter = pos;
                     prevBlue = pos.copy();
@@ -64,15 +69,18 @@ const sketch = (p) => {
     }
 
     p.draw = () => {
-        p.background(0, 40); // Feedback loop: Background doesn't clear fully (Melting look)
+        // This transparency creates the "trailing/melting" Anadol effect
+        p.background(0, 30); 
         
         fluid.update();
 
         for (let part of particles) {
-            // Get the "Fluid Current" from the math file
             let flow = fluid.getVelocity(part.x, part.y);
-            
-            if (greenCenter) part.revealIfNear(greenCenter, 80);
+            if (greenCenter || blueCenter) {
+                // If either hand is near, reveal
+                if (greenCenter && p.dist(part.x, part.y, greenCenter.x, greenCenter.y) < 80) part.visible = true;
+                if (blueCenter && p.dist(part.x, part.y, blueCenter.x, blueCenter.y) < 80) part.visible = true;
+            }
             
             part.applyFlow(flow);
             part.update();
@@ -87,20 +95,25 @@ class ImageParticle {
         this.c = c; this.visible = false;
         this.vx = 0; this.vy = 0;
     }
-    revealIfNear(c, r) { if (this.p.dist(this.x, this.y, c.x, c.y) < r) this.visible = true; }
-    applyFlow(f) { this.vx += f.x * 0.1; this.vy += f.y * 0.1; }
+    applyFlow(f) { 
+        this.vx += f.x * 0.15; 
+        this.vy += f.y * 0.15; 
+    }
     update() {
-        this.x += this.vx; this.y += this.vy;
-        this.vx *= 0.9; this.vy *= 0.9; // Friction
+        this.x += this.vx; 
+        this.y += this.vy;
+        this.vx *= 0.92; // Liquid friction
+        this.vy *= 0.92;
         
-        // Return to origin (The "Dream" elastic effect)
-        this.x += (this.ox - this.x) * 0.01;
-        this.y += (this.oy - this.y) * 0.01;
+        // Elastic pull back to original position
+        this.x += (this.ox - this.x) * 0.02;
+        this.y += (this.oy - this.y) * 0.02;
     }
     show() {
         if (this.visible) {
             this.p.fill(this.c); this.p.noStroke();
-            this.p.rect(this.x, this.y, 4, 4);
+            // Squares render faster and look more like digital data
+            this.p.rect(this.x, this.y, 3, 3);
         }
     }
 }
